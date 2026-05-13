637d33ca735f23dd538f462f80cd9cef9ed7faca
Akhil986 <ganeshakhilmeduri@gmail.com>
2026-05-13 05:36:28 +0000

    Deploying to gh-pages from @ Akhil986/Akhil986.github.io@fc0ff3df560a27871f93775c976ff3c492fce605 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
