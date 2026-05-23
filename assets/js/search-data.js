33fdc7df2cda8c5209862098bdf6319326f7e286
Akhil986 <ganeshakhilmeduri@gmail.com>
2026-05-23 21:18:15 +0000

    Deploying to gh-pages from @ Akhil986/Akhil986.github.io@bae9a8a4b8e46efdcdc6ab8dc94f4aa19a4bad5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,9 +423,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-notes-app",
+          title: 'Notes app',
+          description: "It is a Mern website that is used for making notes",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
