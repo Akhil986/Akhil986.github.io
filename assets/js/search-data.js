93de9902d743a36a5d186f70e092dbe9b455608c
Akhil986 <ganeshakhilmeduri@gmail.com>
2026-05-24 04:12:23 +0000

    Deploying to gh-pages from @ Akhil986/Akhil986.github.io@49cef1b353de2251135eb40552f8dc0107c35117 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,11 +433,6 @@ ninja.data = [{
           description: "A community-driven sports spot sharing platform with interactive maps and discussion forums",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
